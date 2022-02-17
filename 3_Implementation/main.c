9165b53a1d3f986e7f1d76d0acdcfdc037e6654a
rahul <rahuladhikari21@gmai.com>
2022-02-17 20:26:20 +0530

    ok

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -5,7 +5,7 @@
 *******************************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
 #include "stdio.h"
-#include "Calc.h"
+#include "main.h"
 #include "ctype.h"
 
 
